74379030b35ae3542fc5664d07368326f4946def
zhangmrit <zhangmrit@126.com>
2019-06-10 17:49:36 +0800

    登出

--- ruoyi-service/ruoyi-auth/src/main/java/com/ruoyi/auth/controller/TokenController.java
+++ ruoyi-service/ruoyi-auth/src/main/java/com/ruoyi/auth/controller/TokenController.java
@@ -1,5 +1,7 @@
 package com.ruoyi.auth.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.auth.form.LoginForm;
 import com.ruoyi.auth.service.AccessTokenService;
 import com.ruoyi.auth.service.SysLoginService;
+import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.R;
+import com.ruoyi.common.utils.ServletUtils;
 import com.ruoyi.system.domain.SysUser;
 
 @RestController
@@ -27,5 +31,16 @@ public class TokenController
         SysUser user = sysLoginService.login(form.getUsername(), form.getPassword());
         // 获取登录token
         return R.ok(tokenService.createToken(user.getUserId(), user.getLoginName(), user.getPassword()));
+    }
+
+    @PostMapping("logout")
+    public R logout(HttpServletRequest request)
+    {
+        Long userId = Long.valueOf(ServletUtils.getRequest().getHeader(Constants.USER_KEY));
+        if (null != userId)
+        {
+            tokenService.expireToken(userId);
+        }
+        return R.ok();
     }
 }
