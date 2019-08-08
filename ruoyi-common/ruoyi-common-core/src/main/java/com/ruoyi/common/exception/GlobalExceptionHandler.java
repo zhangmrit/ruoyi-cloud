9e21f04b95dff8e980d0fae1322e7dc3fb004a61
zhangmrit <zhangmrit@126.com>
2019-08-08 18:52:06 +0800

    forbidden

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/exception/GlobalExceptionHandler.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/exception/GlobalExceptionHandler.java
@@ -17,66 +17,74 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @author lucas
  */
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-	private Logger logger = LoggerFactory.getLogger(getClass());
+public class GlobalExceptionHandler
+{
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	/**
-	 * 请求方式不支持
-	 */
-	@ExceptionHandler({HttpRequestMethodNotSupportedException.class})
-	@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
-	public R handleException(HttpRequestMethodNotSupportedException e) {
-		logger.error(e.getMessage(), e);
-		return R.error("不支持' " + e.getMethod() + "'请求");
-	}
+    /**
+     * 请求方式不支持
+     */
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    @ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
+    public R handleException(HttpRequestMethodNotSupportedException e)
+    {
+        logger.error(e.getMessage(), e);
+        return R.error("不支持' " + e.getMethod() + "'请求");
+    }
 
-	/**
-	 * 拦截未知的运行时异常
-	 */
-	@ExceptionHandler(RuntimeException.class)
-	public R notFount(RuntimeException e) {
-		if (AnnotationUtils.findAnnotation
-			(e.getClass(), ResponseStatus.class) != null) {
-			throw e;
-		}
-		logger.error("运行时异常:", e);
-		return R.error("运行时异常:" + e.getMessage());
-	}
+    /**
+     * 拦截未知的运行时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public R notFount(RuntimeException e)
+    {
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
+        {
+            throw e;
+        }
+        logger.error("运行时异常:", e);
+        return R.error("运行时异常:" + e.getMessage());
+    }
 
-	/**
-	 * 处理自定义异常
-	 */
-	@ExceptionHandler(RuoyiException.class)
-	public R handleWindException(RuoyiException e) {
-		return R.error(e.getCode(), e.getMessage());
-	}
+    /**
+     * 处理自定义异常
+     */
+    @ExceptionHandler(RuoyiException.class)
+    public R handleWindException(RuoyiException e)
+    {
+        return R.error(e.getCode(), e.getMessage());
+    }
 
-	@ExceptionHandler(DuplicateKeyException.class)
-	public R handleDuplicateKeyException(DuplicateKeyException e) {
-		logger.error(e.getMessage(), e);
-		return R.error("数据库中已存在该记录");
-	}
+    @ExceptionHandler(DuplicateKeyException.class)
+    public R handleDuplicateKeyException(DuplicateKeyException e)
+    {
+        logger.error(e.getMessage(), e);
+        return R.error("数据库中已存在该记录");
+    }
 
-	@ExceptionHandler(Exception.class)
-	public R handleException(Exception e) throws Exception {
-		logger.error(e.getMessage(), e);
-		return R.error("服务器错误，请联系管理员");
-	}
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e) throws Exception
+    {
+        logger.error(e.getMessage(), e);
+        return R.error("服务器错误，请联系管理员");
+    }
 
-//	/**
-//	 * 捕获并处理未授权异常
-//	 *
-//	 * @param e 未授权异常
-//	 * @return 统一封装的结果类, 含有代码code和提示信息msg
-//	 */
-//	@ExceptionHandler(UnauthorizedException.class)
-//	public R handle401(UnauthorizedException e) {
-//		return R.error(401, e.getMessage());
-//	}
+    /**
+     * 捕获并处理未授权异常
+     *
+     * @param e 授权异常
+     * @return 统一封装的结果类, 含有代码code和提示信息msg
+     */
+    @ExceptionHandler(UnauthorizedException.class)
+    public R handle401(UnauthorizedException e)
+    {
+        return R.error(401, e.getMessage());
+    }
 
-	// 验证码错误
-	@ExceptionHandler(ValidateCodeException.class)
-	public R handleCaptcha(ValidateCodeException e) {
-		return R.error(e.getMessage());
-	}
+    // 验证码错误
+    @ExceptionHandler(ValidateCodeException.class)
+    public R handleCaptcha(ValidateCodeException e)
+    {
+        return R.error(e.getMessage());
+    }
 }
