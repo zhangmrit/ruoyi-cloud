6e9c005b2a3eb292f5e05be6e4a096a9c655b2b0
wind <zhangmrit@126.com>
2020-01-13 17:26:51 +0800

    act 流程图修复jar 和业务sql

---
+++ sql/activiti.sql
@@ -0,0 +1,90 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : ruoyi-cloud
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : 120.78.129.95:3306
+ Source Schema         : activiti
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 13/01/2020 17:14:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for biz_audit
+-- ----------------------------
+DROP TABLE IF EXISTS `biz_audit`;
+CREATE TABLE `biz_audit`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '任务编号',
+  `result` tinyint(3) UNSIGNED NOT NULL COMMENT '审核结果 2通过 3驳回',
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '审核意见',
+  `proc_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '流程名称',
+  `proc_def_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '路由 流程定义key',
+  `applyer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '申请人',
+  `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `del_flag` bit(1) NOT NULL DEFAULT b'0' COMMENT '删除标记',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 70 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '业务审核记录' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for biz_business
+-- ----------------------------
+DROP TABLE IF EXISTS `biz_business`;
+CREATE TABLE `biz_business`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `proc_def_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '流程定义编号',
+  `proc_def_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '流程定义key 路由标记',
+  `proc_inst_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '流程实例编号',
+  `proc_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '流程名称',
+  `current_task` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '当前任务节点名称',
+  `result` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '结果状态  1处理中 2通过 3驳回',
+  `status` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态 1处理中 2结束',
+  `table_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '关联表id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '申请标题',
+  `user_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '创建用户id',
+  `applyer` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '申请人',
+  `apply_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '申请时间',
+  `del_flag` bit(1) NULL DEFAULT b'0' COMMENT '删除标记',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 82 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for biz_leave
+-- ----------------------------
+DROP TABLE IF EXISTS `biz_leave`;
+CREATE TABLE `biz_leave`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `start_date` datetime(0) NULL DEFAULT NULL COMMENT '开始时间',
+  `end_date` datetime(0) NULL DEFAULT NULL COMMENT '结束时间',
+  `duration` double(11, 2) NULL DEFAULT NULL COMMENT '时长(小时)',
+  `type` tinyint(3) NULL DEFAULT NULL COMMENT '请假类型',
+  `del_flag` bit(1) NULL DEFAULT b'0' COMMENT '删除标记',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 80 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '请假' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for biz_purchase
+-- ----------------------------
+DROP TABLE IF EXISTS `biz_purchase`;
+CREATE TABLE `biz_purchase`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `title` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `money` decimal(11, 2) UNSIGNED NOT NULL COMMENT '金额',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `del_flag` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '删除标记',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '报销' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
