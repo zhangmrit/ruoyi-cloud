b4cf1ff607d36635f637fda5411eb2c5aacf4b30
wind <zhangmrit@126.com>
2020-03-13 17:06:48 +0800

    升级springcloud到H版并升级monitor监控

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/handler/HystrixFallbackHandler.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/handler/HystrixFallbackHandler.java
@@ -1,5 +1,7 @@
 package com.ruoyi.gateway.handler;
 
+import com.alibaba.fastjson.JSON;
+import com.ruoyi.common.core.domain.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,7 +25,7 @@ public class HystrixFallbackHandler implements HandlerFunction<ServerResponse>
     {
         Optional<Object> originalUris = serverRequest.attribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
         originalUris.ifPresent(originalUri -> log.error("网关执行请求:{}失败,hystrix服务降级处理", originalUri));
-        return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).contentType(MediaType.TEXT_PLAIN)
-                .body(BodyInserters.fromObject("服务异常"));
+        return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(JSON.toJSONString(R.error("服务已被降级熔断"))));
     }
 }
