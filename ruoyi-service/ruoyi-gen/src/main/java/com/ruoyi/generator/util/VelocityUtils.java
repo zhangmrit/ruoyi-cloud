5e56a3371e5ca39a8c667f052882f6112a77b665
wind <zhangmrit@126.com>
2020-05-27 09:41:07 +0800

    comment fix #I1IHAI 格式化

--- ruoyi-service/ruoyi-gen/src/main/java/com/ruoyi/generator/util/VelocityUtils.java
+++ ruoyi-service/ruoyi-gen/src/main/java/com/ruoyi/generator/util/VelocityUtils.java
@@ -169,7 +169,7 @@ public class VelocityUtils
         }
         else if (template.contains("api.js.vm"))
         {
-            fileName = StringUtils.format("{}/api/{}/{}.js", vuePath, moduleName, businessName);
+            fileName = StringUtils.format("{}/api/{}/{}.js", vuePath, moduleName, className);
         }
         else if (template.contains("sql.vm"))
         {
